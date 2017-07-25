2319b35a19d670afc4662c6c8ec31f0f549c9f07
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-25 11:24:47 +0200

    scroll-css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var reponse=["comment ca va..?",
 "je mets de la biere au frais ",
 "ouai un kebab et une bouteille de boulaouane",
+"tu rentre à quelle heure",
+"elle sera pas fraiche",
 "ok à tout à l'heure"];
-var x= -1;
-$(document).ready(function(){
+var x= 0;
+
   $(".button").click(function(){
 
       var contenu =$(".message").val();
@@ -11,9 +13,8 @@ $(document).ready(function(){
        $(".user").append("<p>" + contenu + "</p>" );
        $(".message").val(" ");
        setTimeout (function(){
-       $(".computer" ).append("<p>" + reponse[x] + "</p>" );},2000);
+       $(".computer" ).append("<p>" + reponse[x] + "</p>" );},2500);
        x++;
        console.log(x);
 
     });
-});
